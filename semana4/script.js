1e7e287c5ea8cdfbbfcdee7e961158dfa2aff294
Elizabeth-Souza <elizabethbarbosa691@gmail.com>
2020-08-07 16:44:06 -0300

    Exercícios JS

---
+++ semana4/script.js
@@ -0,0 +1,18 @@
+//----------------------------------------------- BLOCO 1----------------------------------------------------
+
+//Exercícios de interpretação de código
+
+// 1. 
+
+// 2.
+//É um programa para ver o valor de um investimento. 
+// Ele começa com uma função investeDinheiro com dois parâmetros tipoDeInvestimento (poupança, Renda fixa, etc), 
+// e uma let valorAposInvestimento. Depois começa um switch com o parâmetro tipoDeInvestimento com cases com os nomes dos investimentos
+// com a variável valorAposInvestimento para pegar o valor (no caso o valor que a pessoa vai investir) vezes a porcentagem que o investimento acrescenta no valor.
+// No final tem um return que retorna o resultado da multiplicação.
+// Fora da função  tem duas consts para escolher o investimento e indicar o valor que o usuário vai investir.
+// Na primeira a pessoa vai investir em Ações com o valor de 150, no console vai ser impresso o resultado da multiplicação.
+// Na segunda a pessoa vai investir no Tesouro direto no valor de 200, como não tem esse investimento vai ser impresso no console 
+// TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+
+// 3.
